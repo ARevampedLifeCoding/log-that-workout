3edd028ea201a18621075d97b16f8c8b99f59e72
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-02 11:34:43 -0400

    set up exporting the Workout page

---
+++ models/Workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  exercises: [{
+    type: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    duration: {
+      type: Number,
+      required: false
+    },
+    weight: {
+      type: Number,
+      required: false
+    },
+    reps: {
+      type: Number,
+      required: false
+    },
+    sets: {
+      type: Number,
+      required: false
+    },
+    distance: {
+      type: Number,
+      required: false
+    }
+  }],
+  day: {
+    type: Date,
+    default: Date.now
+  },
+})
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
