e69bf02efed23c002e4443041780c4939854227e
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-04 10:00:33 -0400

    db

--- server.js
+++ server.js
@@ -11,12 +11,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/log-that-workout", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/log-that-workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 app.use(require("./routes/apiROutes.js"));
 app.use(require("./routes/mainRoutes.js"));
